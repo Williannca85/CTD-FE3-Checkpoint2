4245b1970ee3ffa94fb922ea981e605fd1a70701
Veronica Rizzi <vevarizzi@gmail.com>
2022-12-15 16:51:28 -0300

    MainRoutes, fetchs e Link

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
+import { useToken } from "../hooks/useToken";
 import styles from "./Form.module.css";
 
 const LoginForm = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const {changeToken} = useToken();
+
+  const {theme, handleTheme} = useTheme();
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = {
+      username: login,
+      password: password
+    }
     //Nesse handlesubmit você deverá usar o preventDefault,
     //enviar os dados do formulário e enviá-los no corpo da requisição 
     //para a rota da api que faz o login /auth
@@ -9,6 +25,31 @@ const LoginForm = () => {
     //no localstorage para ser usado em chamadas futuras
     //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
     //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
+
+    const requestHeaders = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+
+    const requestConfig = {
+      method: 'POST',
+      headers: requestHeaders,
+      body: JSON.stringify(data)
+    }
+
+    fetch(`http://dhodonto.ctdprojetos.com.br/auth`, requestConfig)
+      .then(response => {
+        if (response.status === 200) {
+          response.json().then(data => {
+            changeToken(data.token)
+            console.log(data.token)
+            navigate("/home")
+          })
+
+        } else {
+          alert("Verifique suas credenciais")
+        }
+      });
   };
 
   return (
@@ -16,7 +57,7 @@ const LoginForm = () => {
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
       <div
-        className={`text-center card container ${styles.card}`}
+        className={`text-center card-${theme} container ${styles.card}`}
       >
         <div className={`card-body ${styles.CardBody}`}>
           <form onSubmit={handleSubmit}>
@@ -25,6 +66,7 @@ const LoginForm = () => {
               placeholder="Login"
               name="login"
               required
+              onChange={event => setLogin(event.target.value)}
             />
             <input
               className={`form-control ${styles.inputSpacing}`}
@@ -32,9 +74,10 @@ const LoginForm = () => {
               name="password"
               type="password"
               required
+              onChange={event => setPassword(event.target.value)}
             />
-            <button className="btn btn-primary" type="submit">
-              Send
+            <button className="btn btn-primary" type="submit" onClick={event => handleSubmit(event)}>
+              Enviar
             </button>
           </form>
         </div>
